19c48d63d9d9473faff54830a01cee69ca5749d4
Gabriel Erbetta <gabrielerbetta@gmail.com>
2015-12-11 10:33:48 -0300

    Changed db to PG

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,17 @@
 
 ActiveRecord::Schema.define(version: 20151211020017) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string   "short_url"
     t.string   "url"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-    t.integer  "accesses",   default: 0
+    t.integer  "accesses",   default: 0, null: false
   end
 
-  add_index "urls", ["short_url"], name: "index_urls_on_short_url", unique: true
+  add_index "urls", ["short_url"], name: "index_urls_on_short_url", unique: true, using: :btree
 
 end
